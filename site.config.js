e27fca26f07b6ad5f9e5b9edd3e58f8e46d0ca6d
Trevor Atlas <me@trevoratlas.com>
2021-07-26 17:05:21 -0400

    init

---
+++ site.config.js
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: `@trevoratlas`,
+  title: `Understanding Typescript`,
+  date: `July 26th, 2021`,
+  author: {
+    url: `https://twitter.com/trevoratlas`,
+  },
+}
+
+export default siteConfig
